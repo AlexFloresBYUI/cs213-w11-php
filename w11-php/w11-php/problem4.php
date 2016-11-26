1d708823ba90bc8c3d1eec9ba36cb3d433985af8
Alejandro Flores <ckoolander@gmail.com>
2016-11-25 22:37:38 -0600

    Added problem4.php

---
+++ w11-php/w11-php/problem4.php
@@ -0,0 +1,26 @@
+<?php
+$numbers[0] = cero;
+$numbers[1] = uno;
+$numbers[2] = dos;
+$numbers[3] = tres;
+$numbers[4] = cuatro;
+$numbers[5] = cinco;
+$numbers[6] = seis;
+$numbers[7] = siete;
+$numbers[8] = ocho;
+$numbers[9] = nueve;
+$numbers[10] = diez;
+$numbers[11] = once;
+$numbers[12] = doce;
+$numbers[13] = trece;
+$numbers[14] = catorce;
+$numbers[15] = quince;
+$numbers[16] = dieciseis;
+$numbers[17] = diecisiete;
+$numbers[18] = dieciocho;
+$numbers[19] = diecinueve;
+$numbers[20] = veinte;
+
+echo "The word \"" .$_POST['input4'] ;
+$_POST["input4"];
+?>
